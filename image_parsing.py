db345114b6e6d381256836a0ce262e0c2153e3a7
tuan_vu <tuanvu0695@gmail.com>
2019-02-05 17:48:08 +0000

    Updated

--- image_parsing.py
+++ image_parsing.py
@@ -14,39 +14,60 @@ with open('config.json') as f:
   data = json.load(f)
 pprint(data)
 
-def add_repo_to_image(img):
+image_tag_source = data["image"]
+
+def add_repo_to_image(img,source):
   print ("begin adding repo to image")
 
   with open('config.json') as f:
-      data = json.load(f)
+    data = json.load(f)
   pprint(data)
-  
-  print (str(img['source']).__contains__('http'))
 
-  if (str(img['source']).__contains__('http')) == True:
+  print(img)
+
+  print(source)
+
+  print (str(img[source]))
+
+  print (str(img[source]).__contains__('http'))
+
+  if (str(img[source]).__contains__('http')) == True:
       print("image is pulled from the Internet, leave it")
-  else:
+  else:      
       print("change the source of image to S3")
       repo = str(data["repo"]) + "/" + str(data["campaign"][0]["name"]) + "-" + str(data["width"]) + "x" + str(data["height"]) + "/" + str(data["DSP"]) + "/" + str(data["build"])
+      img[source] = (repo + "/" + img[source])
+      print(img[source])
 
-      img['source'] = (repo + "/" + img['source'])
-
-      print(img['source'])
-
-image_tag_source = data["image"]
+# print (soup.find_all("img",{"src":True}))
 
 for image_tag in image_tag_source:
-    print(image_tag)
-    image = soup.find_all(image_tag)
-    if not image:
-        print ("no image tag")
-    else:
-        print ("has image tag")	        
-        exist_image = image
-        for img in image:
-            print(img["source"])
-            add_repo_to_image(img)
+  print(image_tag)    
+  # source_image = soup.find_all(image_tag,{"source":True})
+  # scr_image = soup.find_all(image_tag,{"source":True})
+  if (soup.find_all(str(image_tag),{"src":True})):
+    print("found src " + image_tag)
+    for img in (soup.find_all(str(image_tag),{"src":True})):      
+      add_repo_to_image(img,"src")
+  elif (soup.find_all(str(image_tag),{"source":True})):
+    print("found source " + image_tag)
+    for img in (soup.find_all(str(image_tag),{"source":True})):      
+      add_repo_to_image(img,"source")
+  else:
+    print("cant find image source")
+
+  # print(soup.find_all(image_tag,{"src":True}))
+
+
+  # if (soup.find_all(str(image_tag),{"scr":True})):
+  #     print ("image src tag")        
+  # elif (soup.find_all(str(image_tag),{"source":True})):
+  #     print ("image source tag")	        
+  # else:
+  #     print ("no image source src")
+      
 
 f= open(html_file,"w+")
 f.write(str(soup.prettify()))
 f.close()
+

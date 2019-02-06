0f5fc7659fe0266429b1621b83aede0bd0796236
tuan_vu <tuanvu0695@gmail.com>
2019-02-06 04:31:13 +0000

    Updated

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -24,7 +24,7 @@ ls $WORKSPACE
 
 echo s3://tuan.vu.yoose/Campaigns/$campaign-${width}x$height/$DSP/${build}
 
-~/.local/bin/aws s3 cp $WORKSPACE s3://tuan.vu.yoose/Campaigns/$campaign-${width}x$height/$DSP/${build} --recursive --exclude "*" --exclude "*.sh" --acl public-read
+eval $(~/.local/bin/aws s3 cp $WORKSPACE s3://tuan.vu.yoose/Campaigns/$campaign-${width}x$height/$DSP/${build} --recursive --exclude "*" --exclude "*.sh" --acl public-read)
 
 echo $WORKSPACE
 
